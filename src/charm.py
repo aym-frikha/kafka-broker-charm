6522ec5c89c3dbd1870ea57c857827e78c6858f9
Pedro Guimaraes <pedro.guimaraes@canonical.com>
2021-04-13 11:43:28 +0200

    Detailed config generation and moved common features to charms.wand

--- src/charm.py
+++ src/charm.py
@@ -21,9 +21,9 @@ from charmhelpers.core.hookenv import (
     is_leader
 )
 
-from java import KafkaJavaCharmBase
+from wand.contrib.java import KafkaJavaCharmBase
 from cluster import KafkaBrokerCluster
-from zookeeper import ZookeeperRequiresRelation
+from wand.apps.relations.zookeeper import ZookeeperRequiresRelation
 
 logger = logging.getLogger(__name__)
 
@@ -45,7 +45,6 @@ CONFLUENT_PACKAGES = [
 KAFKA_BROKER_SERVICE_TARGET="/lib/systemd/system/{}.service"
 
 class KafkaBrokerCharm(KafkaJavaCharmBase):
-    """Charm the service."""
 
     def _install_tarball(self):
         # Use _generate_service_files here
@@ -62,11 +61,11 @@ class KafkaBrokerCharm(KafkaJavaCharmBase):
     # Used for the archive install_method
     def _generate_service_files(self):
         kafka_broker_service_unit_overrides = yaml.safe_load(
-            self.config.get('archive-service-unit-overrides',""))
+            self.config.get('service-unit-overrides',""))
         kafka_broker_service_overrides = yaml.safe_load(
-            self.config.get('archive-service-overrides',""))
+            self.config.get('service-overrides',""))
         kafka_broker_service_environment_overrides = yaml.safe_load(
-            self.config.get('archive-service-environment-overrides',""))
+            self.config.get('service-environment-overrides',""))
         target = None
         if self.distro == "confluent":
             target = "/lib/systemd/system/confluent-server.service"
@@ -74,8 +73,8 @@ class KafkaBrokerCharm(KafkaJavaCharmBase):
             raise Exception("Not Implemented Yet")
         render(source="kafka-broker.service.j2",
                target=target,
-               user=self.config.get('kafka-broker-user'),
-               group=self.config.get("kafka-broker-group"),
+               user=self.config.get('user'),
+               group=self.config.get("group"),
                perms=0o644,
                context={
                    "kafka_broker_service_unit_overrides": kafka_broker_service_unit_overrides,
@@ -89,6 +88,73 @@ class KafkaBrokerCharm(KafkaJavaCharmBase):
         self.framework.observe(self.on.config_changed, self._on_config_changed)
         self.cluster = KafkaBrokerCluster(self, 'cluster')
         self.zk = ZookeeperRequiresRelation(self, 'zookeeper')
+        self.ks.set_default(zk_cert="")
+        self.ks.set_default(zk_key="")
+        os.makedirs("/var/ssl/private")
+        self._generate_keystores()
+
+    def _create_log_dirs(self):
+        for d in self.config.get("log.dirs","").split(","):
+            try:
+                self.create_log_dir(data_log_dev=None,
+                                    data_log_dir=d,
+                                    data_log_fs=None,
+                                    user=self.config["user"],
+                                    group=self.config["group"])
+            except:
+                # folder already exists
+                pass
+
+    def _generate_keystores(self):
+        if self.config["generate-root-ca"]:
+            generateSelfSigned("/var/lib",
+                               certname="zk-kafka-broker-root-ca",
+                               user=self.config["user"],
+                               group=self.config["group"],
+                               mode=0o640)
+            with open("/var/lib/zk-kafka-broker-root-ca.crt", "r") as f:
+                self.ks.zk_cert = f.read()
+                f.close()
+            with open("/var/lib/zk-kafka-broker-root-ca.key", "r") as f:
+                self.ks.zk_key = f.read()
+                f.close()
+            generateSelfSigned("/var/lib",
+                               certname="ssl-kafka-broker-root-ca",
+                               user=self.config["user"],
+                               group=self.config["group"],
+                               mode=0o640)
+            with open("/var/lib/ssl-kafka-broker-root-ca.crt", "r") as f:
+                self.ks.ssl_cert = f.read()
+                f.close()
+            with open("/var/lib/ssl-kafka-broker-root-ca.key", "r") as f:
+                self.ks.ssl_key = f.read()
+                f.close()
+        else:
+            # Certs already set either as configs or certificates relation
+            self.ks.zk_cert  = get_zk_cert()
+            self.ks.zk_key   = get_zk_key()
+            self.ks.ssl_cert = get_ssl_cert()
+            self.ks.ssl_key  = get_ssl_key()
+        self.ks.ks_zookeeper_pwd = genRandomPassword()
+        self.ks.ts_zookeeper_pwd = genRandomPassword()
+        PKCS12CreateKeystore(
+            self.config.get("keystore-zookeeper-path",
+                            "/var/ssl/private/kafka_zookeeper_ks.jks"),
+            self.ks.ks_zookeeper_pwd,
+            self.get_zk_cert(),
+            self.get_zk_key(),
+            user=self.config["user"],
+            group=self.config["group"],
+            mode=0o640)
+        PKCS12CreateKeystore(
+            self.config.get("keystore-path",
+                            "/var/ssl/private/kafka_ssl_ks.jks"),
+            self.ks.ks_password,
+            self.get_ssl_cert(),
+            self.get_ssl_key(),
+            user=self.config["user"],
+            group=self.config["group"],
+            mode=0o640)
 
     def _on_install(self, _):
         # TODO: Create /var/lib/kafka folder and all log dirs, set permissions
@@ -101,15 +167,36 @@ class KafkaBrokerCharm(KafkaJavaCharmBase):
             else:
                 raise Exception("Not Implemented Yet"
             super().install_packages('openjdk-11-headless', packages)
-        self._on_config_changed()
-
 
     def _rel_get_remote_units(self, rel_name):
         return self.framework.model.get_relation(rel_name).units
 
+    def get_ssl_cert(self):
+        if len(self.ks.ssl_cert) > 0:
+            return self.ks.ssl_cert
+        return base64.b64decode(self.config["ssl_cert"]).decode("ascii")
+
+    def get_ssl_key(self):
+        if len(self.ks.ssl_key) > 0:
+            return self.ks.ssl_key
+        return base64.b64decode(self.config["ssl_key"]).decode("ascii")
+
+    def get_zk_cert(self):
+        # TODO(pguimaraes): expand it to count with certificates relation or action cert/key
+        if len(self.ks.zk_cert) > 0:
+            return self.ks.zk_cert
+        return base64.b64decode(self.config["ssl-zk-cert"]).decode("ascii")
+
+    def get_zk_key(self):
+        # TODO(pguimaraes): expand it to count with certificates relation or action cert/key
+        if len(self.ks.zk_key) > 0:
+            return self.ks.zk_key
+        return base64.b64decode(self.config["ssl-zk-key"]).decode("ascii")
+
     def _generate_server_properties(self):
         # TODO: set confluent.security.event.logger.exporter.kafka.topic.replicas
-        server_props = yaml.safe_load(self.config.get("server-properties", "")) or {}
+        server_props = self.config.get("server-properties", "")
+        server_props["log.dirs"] = self.config["log.dirs"]
         if os.environ.get("JUJU_AVAILABILITY_ZONE") and self.config["customize-failure-domain"]:
             server_props["broker.rack"] = os.environ.get("JUJU_AVAILABILITY_ZONE")
         replication_factor = self.config.get("replication-factor",3)
@@ -128,26 +215,125 @@ class KafkaBrokerCharm(KafkaJavaCharmBase):
         server_props = { **server_props, **self.cluster.listeners }
         # TODO: Enable rest proxy if we have RBAC: https://github.com/confluentinc/cp-ansible/blob/b711fc9e3b43d2069a9ac8b13177e7f2a07c7bfb/VARIABLES.md
         server_props["kafka_broker_rest_proxy_enabled"] = False
+
+        # Zookeeper options:
+        server_props["zookeeper.connect"] = self.zk.get_zookeer_list
+        server_props["zookeeper.set.acl"] = zookeeper.clientCnxnSocket()
+        if self.zk.is_mTLS_enabled():
+            # TLS client properties uses the same variables
+            # Rendering as a part of server properties
+            client_props = {}
+            client_props["zookeeper.clientCnxnSocket"] = "org.apache.zookeeper.ClientCnxnSocketNetty"
+            client_props["zookeeper.ssl.client.enable"] = "true"
+            client_props["zookeeper.ssl.keystore.location"] = \
+                self.config.get("keystore-zookeeper-path",
+                                "/var/ssl/private/kafka_zookeeper_ks.jks")
+            self.ks.ks_zookeeper_pwd = genRandomPassword()
+            client_props["zookeeper.ssl.keystore.password"] = self.ks.ks_zookeeper_pwd
+            client_props["zookeeper.ssl.truststore.location"] = \
+                self.config.get("truststore-zookeeper-path",
+                                "/var/ssl/private/kafka_zookeeper_ts.jks")
+            self.ks.ts_zookeeper_pwd = genRandomPassword()
+            client_props["zookeeper.ssl.truststore.password"] = self.ks.ts_zookeeper_pwd
+            # Set the SSL mTLS config on the relation
+            self.zk.set_mTLS_auth(self.get_zk_cert(),
+                                  client_props["zookeeper.ssl.truststore.location"],
+                                  self.ks.ts_password)
+            # Attaching client_props to server_props config
+            server_props = {**server_props, **client_props}
+            render(source="zookeeper-tls-client.properties.j2",
+                   target="/etc/kafka/zookeeper-tls-client.properties",
+                   user=self.config.get('user'),
+                   group=self.config.get("group"),
+                   perms=0o640,
+                   context={
+                       "client_props": client_props
+                   })
+        # Back to server.properties, render it
         render(source="server.properties.j2",
                target="/etc/kafka/server.properties",
-               user=self.config.get('kafka-broker-user'),
-               group=self.config.get("kafka-broker-group"),
+               user=self.config.get('user'),
+               group=self.config.get("group"),
                perms=0o640,
                context={
                    "server_props": server_props
                })
 
+    def is_sasl_enabled(self):
+        if self.is_sasl_kerberos_enabled() or \
+           self.is_sasl_oauthbearer_enabled() or \
+           self.is_sasl_scram_enabled() or \
+           self.is_sasl_plain_enabled() or \
+           self.is_sasl_delegate_token_enabled() or \
+           self.is_sasl_ldap_enabled():
+            return True
+        return False
+
     def _generate_client_properties(self):
         # TODO: it seems we need this just when it comes to client encryption
         # https://github.com/confluentinc/cp-ansible/blob/b711fc9e3b43d2069a9ac8b13177e7f2a07c7bfb/roles/confluent.variables/filter_plugins/filters.py#L159
-        pass
+        client_props = self.config["client-properties"] or {}
+        if self.is_sasl_enabled():
+            client_props["sasl.jaas.config"] = self.config.get("sasl-jaas-config","")
+        if self.is_sasl_kerberos_enabled():
+            client_prpos["sasl.mechanism"] = "GSSAPI"
+            client_props["sasl.kerberos.service.name"] = self.config.get("sasl-kbros-service", "HTTP")
+        if self.is_ssl_enabled():
+            client_props["ssl.keystore.location"] = \
+                self.config.get("keystore-path",
+                                "/var/ssl/private/kafka_ks.jks")
+            client_props["ssl.keystore.password"] = self.ks.ks_password
+            client_props["ssl.truststore.location"] = \
+                self.config.get("truststore-path",
+                                "/var/ssl/private/kafka_ts.jks")
+            client_props["ssl.truststore.password"] = self.ks.ts_password
+        render(source="client.properties.j2",
+               target="/etc/kafka/client.properties",
+               user=self.config.get('user'),
+               group=self.config.get("group"),
+               perms=0o640,
+               context={
+                   "client_props": client_props
+               })
+
+#    def _generate_zk_tls_client_properties(self):
+#        client_props = {}
+#        if not self.zk.is_mTLS_enabled():
+#            return
+#        client_props["zookeeper.clientCnxnSocket"] = "org.apache.zookeeper.ClientCnxnSocketNetty"
+#        client_props["zookeeper.ssl.client.enable"] = "true"
+#        client_props["zookeeper.ssl.keystore.location"] = \
+#            self.config.get("keystore-zookeeper-path",
+#                            "/var/ssl/private/kafka_zookeeper_ks.jks")
+#        client_props["zookeeper.ssl.keystore.password"] = self.ks.ks_password
+#        client_props["zookeeper.ssl.truststore.location"] = \
+#            self.config.get("truststore-zookeeper-path",
+#                            "/var/ssl/private/kafka_zookeeper_ts.jks")
+#        client_props["zookeeper.ssl.truststore.password"] = self.ks.ts_password
+#        render(source="zookeeper-tls-client.properties.j2",
+#               target="/etc/kafka/zookeeper-tls-client.properties",
+#               user=self.config.get('user'),
+#               group=self.config.get("group"),
+#               perms=0o640,
+#               context={
+#                   "client_props": client_props
+#               })
+
 
     def _on_config_changed(self, _):
         # Note: you need to uncomment the example in the config.yaml file for this to work (ensure
         # to not just leave the example, but adapt to your configuration needs)
-        logger.debug("found a new thing: %r", current)
-        self._generate_service_files()
+        self._generate_keystores()
         self._generate_server_properties()
+        self._generate_client_properties()
+#        self._generate_zk_tls_client_properties()
+        target = None
+        if self.distro == "confluent":
+            target = "/etc/systemd/system/" + \
+                     "confluent-server.service.d/override.conf"
+        elif self.distro == "apache":
+            raise Exception("Not Implemented Yet")
+        self.render_service_override_file(target)
         # Apply sysctl
 
 
--- src/cluster.py
+++ src/cluster.py
@@ -1,6 +1,8 @@
 import os
 import yaml
+from socket import gethostname
 from ops.framework import Object, StoredState
+from charmhelpers.contrib.network.ip import get_hostname
 
 class KafkaBrokerCluster(Object):
 
@@ -9,6 +11,7 @@ class KafkaBrokerCluster(Object):
     def __init__(self, charm, relation_name):
         super().__init__(charm, relation_name)
         self._charm = charm
+        self._unit  = unit
         self._relation_name = relation_name
         self._relation = self.framework.model.get_relation(self._relation_name)
         self.framework.observe(charm.on.cluster_relation_changed, self)
@@ -17,7 +20,53 @@ class KafkaBrokerCluster(Object):
         self.state.set_default(listeners="")
         self.state.set_default(listener_protocol_map="")
         self.state.set_default(advertised_listeners="")
+        self.state.set_default(keystore_path="/var/lib/private/kafka-broker-ssl-keystore.jks")
+        self.state.set_default(keystore_pwd="")
+        self.state.set_default(truststore_path="/var/ssl/private/kafka-broker-ssl-truststore.jks")
+        self.state.set_default(truststore_pwd="")
+        self.state.set_default(trusted_certs="")
 
+
+    def set_ssl_keypair(self,
+                        ssl_cert,
+                        ks_path,
+                        ks_pwd):
+        self._relation.data[self._unit]["tls_cert"] = ssl_cert
+        self.state.keystore_path = ks_path
+        self.state.keystore_pwd  = ks_pwd
+        self._get_all_tls_certs()
+
+    def is_ssl_enabled(self):
+        if len(self._relation.data[self._unit].get("tls_cert", "")) > 0:
+            return True
+        return False
+
+    @property
+    def keystore(self):
+        return self.state.keystore_path
+
+    @property
+    def keystore_pwd(self):
+        return self.state.keystore_pwd
+
+    @property
+    def truststore_pwd(self):
+        return self.state.truststore_pwd
+
+    @property
+    def truststore(self):
+        return self.state.truststore_path
+
+    def _get_all_tls_certs(self):
+        if not self.is_ssl_enabled():
+            return
+        self.state.trusted_certs = self._relation.data[self._unit]["tls_cert"] + \
+            " " + " ".join(
+                [self._relation.data[u].get("tls_cert", "") for u in self._relation.units])
+        CreateTruststore(self.state.truststore_path,
+                         self.state.truststore_pwd,
+                         self.state.trusted_certs.split(),
+                         ts_regenerate=True)
 
     @property
     def _relations(self):
@@ -35,13 +84,27 @@ class KafkaBrokerCluster(Object):
 
     @property
     def listeners(self):
-        return {
+        listener_opts = {
             "listeners": self.state.listeners,
             "listener.security.protocol.map": self.state.listener_protocol_map,
             "advertised.listeners": self.state.advertised_listeners,
             "inter.broker.listener.name": "INTERNAL",
             "control.plane.listener.name": "CONTROLLER"
         }
+        if is_ssl_enabled():
+            ssl_opts = {}
+            for l in self.state.listeners.split(","):
+                name = "listener.name." + l.split("://")[0]
+                _opts = {}
+                _opts[name + ".ssl.truststore.location"] = self.state.truststore_path
+                _opts[name + ".ssl.truststore.password"] = self.state.truststore_pwd
+                _opts[name + ".ssl.truststore.typee"] = "JKS"
+                _opts[name + ".ssl.keystore.location"] = self.state.keystore_path
+                _opts[name + ".ssl.keystore.password"] = self.state.keystore_pwd
+                _opts[name + ".ssl.keystore.typee"] = "JKS"
+                _pots[name + ".ssl.client.auth"] = "required"
+                ssl_opts = { **_opts, **ssl_opts }
+            listener_opts = { **ss_opts, **listener_opts }
 
     @property
     def is_single(self):
@@ -52,11 +115,7 @@ class KafkaBrokerCluster(Object):
         return self._relation is not None
 
     def on_cluster_relation_joined(self, event):
-        # A workaround for LP: #1859769.
-        if not self.is_joined:
-            event.defer()
-            return
-        # TODO: keep this method, we will likely needed it for later
+        pass
 
     def on_cluster_relation_changed(self, event):
         if os.environ.get("JUJU_AVAILABILITY_ZONE"):
@@ -65,20 +124,31 @@ class KafkaBrokerCluster(Object):
         for u in self._relation.units:
             az_set.add(self._relation.data[u]["az"])
         self.state.peer_num_azs = len(az_set)
-
+        # Resolve hostname
+        hostname = "{}.{}".format(socket.gethostname(), self.config["cluster-domain"]) \
+                   if self.config["cluster-domain"] else get_hostname(self.binding_addr())
         # TODO: ssl_enabled and sasl: add to the listeners
-        ssl_enabled = True if len(self._charm.config.get("ssl_cert") > 0 else False
+        ssl_enabled = self.is_ssl_enabled()
         sasl_protocol = self._charm.config.get("sasl_protocol", False)
         # Creates a list similar to: [{'test': [{'a': 'b', 'c': 'd'}]}]
-        listeners = yaml.safe_load(self._charm.config.get("listeners",""))[0] or {}
-        listeners["INTERNAL"] =   "{}:{}".format(self.binding_addr(), 9092)
-        listeners["CONTROLLER"] = "{}:{}".format(self.binding_addr(), 9093)
-        listeners["EXTERNAL"] =   "{}:{}".format(self.advertise_addr(),9094)
+        listeners = self._charm.config.get("listeners","") or {}
+        # If this is set via option, we override those values to predefined on charms
+        listeners["INTERNAL"] =   "{}:{}".format(hostname, 9092)
+        listeners["CONTROLLER"] = "{}:{}".format(hostname, 9093)
+        listeners["EXTERNAL"] =   "{}:{}".format(hostname, 9094)
         self.state.listeners = ",".join( [ k+"://"+v for k,v in listeners ] )
-        # TODO: get a second advertised listener list
-        self.state.adversited_listeners = ",".join( [ k+"://"+v for k,v in listeners ] )
-        # TODO: avoid PLAINTEXT
-        self.state.listener_protocol_map = ",".join( [ k+":PLAINTEXT" for k,v in listeners ] )
+        # TODO: avoid PLAINTEXT, check:
+        # https://docs.confluent.io/platform/current/installation/configuration/broker-configs.html#brokerconfigs_listener.security.protocol.map
+        if not ssl_enabled:
+            self.state.listener_protocol_map = ",".join( [ k+":PLAINTEXT" for k,v in listeners ] )
+        else:
+            self.state.listener_protocol_map = ",".join( [ k+":SSL" for k,v in listeners ] )
+        # if advertised listeners is set, pass it here
+        if self._charm.config.get("advertised.listeners", None):
+            self.state.advertised_listeners = self.state.advertised_listeners + \
+                self._charm.config["advertised.listeners"]
+        else:
+            self.state.advertised_listeners = self.state.listeners["EXTERNAL"]
 
     @property
     def peer_addresses(self):
