9f458342412bf782a5014d683571bcdabc946861
Pedro Guimaraes <pedro.guimaraes@canonical.com>
2022-06-20 22:23:59 +0200

    Fixes to support upstream snap

--- src/charm.py
+++ src/charm.py
@@ -1259,7 +1259,9 @@ class KafkaBrokerCharm(KafkaJavaCharmBase):
             svc_opts = self.render_service_override_file(
                 target="/etc/systemd/system/"
                        "{}.service.d/override.conf".format(self.service),
-                jmx_jar_folder="/snap/kafka/current/jar/",
+                jmx_jar_folder = \
+                    "/opt/prometheus/" if self.distro != "apache_snap" \
+                    else "/snap/{}/current/jar/opt/kafka/extra/".format(self.snap),
                 jmx_file_name="/var/snap/kafka/common/prometheus.yaml")
         else:
             svc_opts = self.render_service_override_file(
